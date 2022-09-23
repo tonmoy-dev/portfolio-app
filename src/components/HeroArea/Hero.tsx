8a6efc8bc5d90e696895f99ab2a850d4239e09c7
Tonmoy Roy <rxdevs100@gmail.com>
2022-09-23 19:21:44 +0600

    error resolved

--- src/components/HeroArea/Hero.tsx
+++ src/components/HeroArea/Hero.tsx
@@ -10,7 +10,7 @@ const Hero : NextPage = () => {
                 <h3 className='text-3xl'>I build things for the web.</h3>
             </div>
             <div className='w-1/2'>
-                <p className='text-base'>I'm a full stack web developer specializing in building single page web applications. Currently, I'm focused on building more large scale web apps amd learning more in this field.</p>
+                <p className='text-base'>I&apos;m a full stack web developer specializing in building single page web applications. Currently, I&apos;m focused on building more large scale web apps amd learning more in this field.</p>
             </div>
             <button className='px-4 py-2 border rounded'>Check out my resume!</button>
             </div>
